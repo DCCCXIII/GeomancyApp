613b216c92282e204eee03da4d3330d0fa700f33
Rubén Ruiz Marrón <rubenruizmarron@gmail.com>
2018-09-03 23:57:16 -0500

    Template for twelve houses chart

--- GeomancyApp/Classes/TwelveHouses.cs
+++ GeomancyApp/Classes/TwelveHouses.cs
@@ -3,14 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeomancyApp.Figures;
 
 namespace GeomancyApp.Charts
 {
     class TwelveHouses : Chart
     {
+        public Figure firstHouse;
+        public Figure secondHouse;
+        public Figure thirdHouse;
+        public Figure fourthHouse;
+        public Figure fifthHouse;
+        public Figure sixthHouse;
+        public Figure seventhHouse;
+        public Figure eigthHouse;
+        public Figure ninthHouse;
+        public Figure tenthHouse;
+        public Figure eleventhHouse;
+        public Figure twelfthHouse;
+
+        private string[] template = new string[]
+        {
+            "##########################################################################",   // 0
+            "####                              ##  ##                              ####",   // 1
+            "##  ##                          ##      ##                          ##  ##",   // 2
+            "##    ##                      ##          ##                      ##    ##",   // 3
+            "##      ##                  ##              ##                  ##      ##",   // 4
+            "##        ##              ##                  ##              ##        ##",   // 5
+            "##          ##          ##                      ##          ##          ##",   // 6
+            "##            ##      ##                          ##      ##            ##",   // 7
+            "##              ##  ##                              ##  ##              ##",   // 8
+            "##                ######################################                ##",   // 9
+            "##              ####                                  ####              ##",   // 10
+            "##            ##  ##                                  ##  ##            ##",   // 11
+            "##          ##    ##                                  ##    ##          ##",   // 12
+            "##        ##      ##                                  ##      ##        ##",   // 13
+            "##      ##        ##                                  ##        ##      ##",   // 14
+            "##    ##          ##                                  ##          ##    ##",   // 15
+            "##  ##            ##                                  ##            ##  ##",   // 16
+            "####              ##                                  ##              ####",   // 17
+            "##                ##                                  ##                ##",   // 18
+            "####              ##                                  ##              ####",   // 19
+            "##  ##            ##                                  ##            ##  ##",   // 20
+            "##    ##          ##                                  ##          ##    ##",   // 21
+            "##      ##        ##                                  ##        ##      ##",   // 22
+            "##        ##      ##                                  ##      ##        ##",   // 23
+            "##          ##    ##                                  ##    ##          ##",   // 24
+            "##            ##  ##                                  ##  ##            ##",   // 25
+            "##              ####                                  ####              ##",   // 26
+            "##                ######################################                ##",   // 27
+            "##              ##  ##                              ##  ##              ##",   // 28
+            "##            ##      ##                          ##      ##            ##",   // 29
+            "##          ##          ##                      ##          ##          ##",   // 30
+            "##        ##              ##                  ##              ##        ##",   // 31
+            "##      ##                  ##              ##                  ##      ##",   // 32
+            "##    ##                      ##          ##                      ##    ##",   // 33
+            "##  ##                          ##      ##                          ##  ##",   // 34
+            "####                              ##  ##                              ####",   // 35
+            "##########################################################################",   // 36
+        };
+
         public TwelveHouses(Chart chart)
         {
-
         }
     }
 }
